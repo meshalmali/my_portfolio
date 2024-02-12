d92187ca89fa67e0bbacea631a93b612942239cc
meshalmali <shalmalib15@gmail.com>
2024-02-12 09:11:16 +0530

    add tailwind and update ui

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+
+function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleClick = (section) => {
+    setShowMenu(false);
+    const sectionToScroll = document.getElementById(section);
+    const offset = sectionToScroll.offsetTop - 64;
+
+    window.scrollTo({
+      top: offset,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <>
+      <div className="fixed h-16 p-4 lg:h-20 lg:py-6 lg:px-12 2xl:h-21.5 2xl:py-9 2xl:px-12 w-full bg-transparent backdrop-blur-md z-50 flex justify-between items-center">
+        <a
+          className="flex"
+          href="https://in.linkedin.com/in/shalmali-bhave-601113163"
+          target="_blank"
+        >
+          <span className="handlee-regular text-white text-lg">
+            Shalmali Bhave
+          </span>
+        </a>
+        <div
+          onClick={() => setShowMenu(!showMenu)}
+          className="w-8 h-8 lg:hidden bg-no-repeat bg-center bg-cover cursor-pointer"
+          style={{
+            backgroundImage: `${
+              !showMenu
+                ? `url("/logos/hamburger.svg")`
+                : `url("/logos/close.svg")`
+            }`,
+          }}
+        ></div>
+        {/* *******Larger Screens***** */}
+        <div className="gap-14 text-white playfair-black text-xl hidden lg:flex">
+          <a
+            className="cursor-pointer"
+            onClick={() => handleClick("about-section")}
+          >
+            About
+          </a>
+          <a
+            className="cursor-pointer"
+            onClick={() => handleClick("projects-section")}
+          >
+            My Projects
+          </a>
+          <a
+            className="cursor-pointer"
+            onClick={() => handleClick("skills-section")}
+          >
+            My Skills
+          </a>
+          <a
+            className="cursor-pointer"
+            onClick={() => handleClick("footer-section")}
+          >
+            Let's Connect
+          </a>
+        </div>
+      </div>
+      {showMenu && (
+        <div className="w-full h-60 left-0 top-16 fixed menu-slide bg-white bg-opacity-10 backdrop-blur-lg z-50 flex flex-col items-center justify-center text-white playfair-black text-sm gap-4">
+          <a
+            className="border rounded-xl px-3 py-1 cursor-pointer"
+            onClick={() => handleClick("about-section")}
+          >
+            About
+          </a>
+          <a
+            className="border rounded-xl px-3 py-1 cursor-pointer"
+            onClick={() => handleClick("projects-section")}
+          >
+            My Projects
+          </a>
+          <a
+            className="border rounded-xl px-3 py-1 cursor-pointer"
+            onClick={() => handleClick("skills-section")}
+          >
+            My Skills
+          </a>
+          <a
+            className="border rounded-xl px-3 py-1 cursor-pointer"
+            onClick={() => handleClick("footer-section")}
+          >
+            Let's Connect
+          </a>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Navbar;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,42 +1,62 @@
 import React from "react";
-import "./ProjectCard.css";
-import ProjectCard from "./ProjectCard";
 
 function Projects() {
+  const projects = [
+    {
+      name: "Noogle",
+      description:
+        "A Google clone written in React using Context API, Material UI and Google's custom search API",
+      url: "/projects/noogle.png",
+    },
+    {
+      name: "Let's Chat",
+      description:
+        "A Web Chat Application written in React using Context API and realtime database configured with Firestore.",
+      url: "/projects/weChat.png",
+    },
+    {
+      name: "Admin Arcade",
+      description:
+        "Visualizes data in a user-friendly dashboard interface, with a fancy login screen using Tailwind CSS",
+      url: "/projects/noogle.png",
+    },
+    {
+      name: "New",
+      description: "Something coming up",
+      url: "/projects/noogle.png",
+    },
+  ];
   return (
-    <div className="projects" id="projects">
-      <h1>Check out some of my Projects</h1>
-      <div className="projects_container">
-        <div className="projects_wrapper">
-          <ul className="projects_cards">
-            <ProjectCard
-              src="/projects/noogle.png"
-              text="A Google clone written in React using Context API, Material UI and Google's custom search API"
-              label="Noogle"
-              path="https://noogle-995bb.web.app"
-            />
-            <ProjectCard
-              src="/projects/pexels-marta-branco.jpg"
-              text="A basic cryptocurrency finance price tracker built with React using the Coingecko cryptocurrency data API"
-              label="Crypto Price Tracker"
-              path="https://crypto-price-tracker-d13e9.web.app"
-            />
-          </ul>
-          <ul className="projects_cards">
-            <ProjectCard
-              src="/projects/spotify.png"
-              text="A Spotify Web clone written in React and includes user authentication using the popular Spotify Web API"
-              label="Spotify Clone"
-              path="https://spotify-web-clone-a508d.web.app"
-            />
-            <ProjectCard
-              src="/projects/weChat.png"
-              text="A Web Chat Application written in React using Context API and realtime database configured with Firestore."
-              label="Chat App"
-              path="https://chat-application-e0f03.web.app"
-            />
-          </ul>
-        </div>
+    <div
+      id="projects-section"
+      className="h-full bg-slate-800 flex flex-col gap-8 md:gap-20 2xl:gap-36 items-center p-8 md:py-20 md:px-28 2xl:py-36 2xl:px-0"
+    >
+      <span className="text-3xl md:text-4xl lg:text-5xl text-center playfair-black text-white">
+        Check out some of my Projects
+      </span>
+      <div className="flex flex-col md:items-center lg:flex-row lg:flex-wrap lg:justify-center lg:max-w-[72rem] 2xl:max-w-[80.62rem] w-full gap-6 md:gap-12 lg:gap-20 2xl:gap-28 px-8">
+        {projects.map((project) => {
+          return (
+            <>
+              <div className="h-60 w-full md:max-w-96 md:h-80 lg:w-80 lg:h-96 2xl:max-w-[22.75rem] 2xl:w-[22.75rem] 2xl:h-[25rem] flex flex-col rounded-[50px] shadow-right bg-slate-700 overflow-hidden cursor-pointer">
+                <div className="h-4/6 w-full overflow-hidden relative">
+                  <div
+                    className="bg-no-repeat bg-cover bg-center w-full h-full hover:scale-125 transition ease-in-out delay-150 duration-200"
+                    style={{
+                      backgroundImage: `url(${project.url})`,
+                    }}
+                  ></div>
+                  <div className="absolute shine-recurring bg-gradient-to-r from-teal-700 to-sky-700 bottom-0 text-white playfair-semibold text-base md:2xl lg:text-2xl 2xl:text-3xl p-1 2xl:p-3 rounded-t-lg">
+                    {project.name}
+                  </div>
+                </div>
+                <span className="p-4 2xl:p-8 text-sm md:text-xl lg:text-xl 2xl:text-2xl playfair-normal w-full text-white">
+                  {project.description}
+                </span>
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
